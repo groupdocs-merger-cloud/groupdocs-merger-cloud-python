1ef266880aba49a9cebeaed8040c3a73a0a5c271
product-team <product.team@groupdocs.com>
2024-11-28 02:08:05 -0600

    Updated sources

--- groupdocs_merger_cloud/configuration.py
+++ groupdocs_merger_cloud/configuration.py
@@ -202,6 +202,6 @@ class Configuration(object):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 24.7\n"\
-               "SDK Package Version: 24.7".\
+               "Version of the API: 24.11\n"\
+               "SDK Package Version: 24.11".\
                format(env=sys.platform, pyversion=sys.version)
